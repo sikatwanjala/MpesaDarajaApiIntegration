0f63468ba3146bfe285ba80ad066cdc0d2c66c88
sikata wanjala filex <sikatafilex@gmail.com>
2024-03-20 16:00:07 +0300

    sending stkpush file

--- accessToken.php
+++ accessToken.php
@@ -12,10 +12,9 @@ curl_setopt($curl, CURLOPT_HEADER, FALSE);
 curl_setopt($curl, CURLOPT_USERPWD, $consumerKey . ':' . $consumerSecret);
 $result = curl_exec($curl);
 $status = curl_getinfo($curl, CURLINFO_HTTP_CODE);
-echo $result;
-
-// $result = json_decode($result);
-// echo $access_token = $result->access_token;
+// echo $result;
+$result = json_decode($result);
+$access_token = $result->access_token;
 curl_close($curl);
 
 
