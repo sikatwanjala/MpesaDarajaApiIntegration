4b3593793af223af7865f171e0d001ff98ea8f43
sikata wanjala filex <sikatafilex@gmail.com>
2024-03-21 15:50:31 +0300

    adding stkpush files

--- stkpush.php
+++ stkpush.php
@@ -1,15 +1,16 @@
 <?php
 //INCLUDE THE ACCESS TOKEN FILE
+
 include 'accessToken.php';
 date_default_timezone_set('Africa/Nairobi');
 $processrequestUrl = 'https://sandbox.safaricom.co.ke/mpesa/stkpush/v1/processrequest';
-$callbackurl = 'https://1c95-105-161-14-223.ngrok-free.app/MPEsa-Daraja-Api/callback.php';
+$callbackurl = 'https://1c95-105-161-14-223.ngrok-free.app/projectpresentation/callback.php';
 $passkey = "bfb279f9aa9bdbcf158e97dd71a467cd2e0c893059b10f78e6b72ada1ed2c919";
 $BusinessShortCode = '174379';
 $Timestamp = date('YmdHis');
 // ENCRIPT  DATA TO GET PASSWORD
 $Password = base64_encode($BusinessShortCode . $passkey . $Timestamp);
-$phone = '0706599924';//phone number to receive the stk push
+$phone = '254706599924';//phone number to receive the stk push
 $money = '1';
 $PartyA = $phone;
 $PartyB = '254708374149';
